6c2d2dcd5b18001fe8aa3810510d053fd8985b8d
Wojciech Kalinski <wojciech.kalinski@espeo.eu>
2018-11-28 12:46:30 +0100

    Verified with Remix IDE working regulated contract

--- contracts/Regulated.sol
+++ contracts/Regulated.sol
@@ -3,34 +3,29 @@ pragma solidity ^0.4.24;
 import "./interfaces/RegulatedI.sol";
 
 contract Regulated is RegulatedI {
-
+    
     address internal regulator;
-
-    event LogRegulatorSet(address indexed previousRegulator, address indexed newRegulator);
-
-    modifier onlyIfNotZeroAddress {
-        require(regulator != address(0), "New regulator address cannot be 0!");
-        _;
-    }
-
-    modifier onlyIfNewRegulator (RegulatorI _newRegulator) {
-        require(_newRegulator != regulator, "New regulator cannot be the same");
-        _;
-    }
-
-    constructor(address _regulator) {
-        require(_regulator != 0x0, "New regulator address cannot be 0!");
+    
+    constructor(address _regulator) public {
+        require(_regulator != 0);
         regulator = _regulator;
     }
-
-    function setRegulator(address _newRegulator) public onlyIfNotZeroAddress returns(bool success) {
-        require(_newRegulator != regulator, "New regulator cannot be the same");
-        regulator = _newRegulator;
-        emit LogRegulatorSet(msg.sender, _newRegulator);
+    
+    event LogRegulatorSet(address indexed previousRegulator, address indexed newRegulator);
+    function setRegulator(address newRegulator) public onlyRegulator returns(bool success) {
+        require(newRegulator > 0);
+        require(newRegulator != regulator);
+        emit LogRegulatorSet(regulator, newRegulator);
+        regulator = newRegulator;
         return true;
     }
 
-    function getRegulator() public view returns(RegulatorI) {
+    function getRegulator() public view returns(RegulatorI _regulator) {
         return RegulatorI(regulator);
+    }
+    
+    modifier onlyRegulator() {
+        require(msg.sender == regulator, "Sender must be a regulator of contract");
+        _;
     }
 }
