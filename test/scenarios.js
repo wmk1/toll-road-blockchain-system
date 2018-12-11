f583e9105a974d7540bcb4d2cfba862766bc8370
Wojciech Kalinski <wojciech.kalinski@espeo.eu>
2018-12-11 12:47:40 +0100

    Added working tests scenario

--- test/scenarios.js
+++ test/scenarios.js
@@ -27,113 +27,173 @@ contract('Scenarios', accounts => {
 
   let regulatorInstance
   let tollBoothOperatorInstance
- 
-  beforeEach('Should deploy the necessary contracts', async () => {
-    regulator.instance = await Regulator.new({ from: owner })
+
+  beforeEach('Deploying the necessary contracts before performing tests', async () => {
+    regulator.instance = await Regulator.new({
+      from: owner
+    })
     regulatorInstance = regulator.instance
-    await regulatorInstance.setVehicleType(vehicle, vehicleType, { from: owner })
-    await regulatorInstance.setVehicleType(vehicle1, vehicleType1, { from: owner })
-    let tx = await regulatorInstance.createNewOperator(owner1, deposit, { from: owner })
+    await regulatorInstance.setVehicleType(vehicle, vehicleType, {
+      from: owner
+    })
+    await regulatorInstance.setVehicleType(vehicle1, vehicleType1, {
+      from: owner
+    })
+    let tx = await regulatorInstance.createNewOperator(owner1, deposit, {
+      from: owner
+    })
     tollBoothOperator.instance = await TollBoothOperator.at(tx.logs[1].args.newOperator)
     tollBoothOperatorInstance = tollBoothOperator.instance
-    await tollBoothOperatorInstance.addTollBooth(booth, { from: owner1 })
-    await tollBoothOperatorInstance.addTollBooth(booth1, { from: owner1 })
-    await tollBoothOperatorInstance.setMultiplier(vehicleType, multiplier, { from: owner1 })
-    await tollBoothOperatorInstance.setMultiplier(vehicleType1, multiplier1, { from: owner1 })
-    await tollBoothOperatorInstance.setRoutePrice(booth, booth1, price, { from: owner1 })
-    hashed = tollBoothOperatorInstance.hashSecret(secret)
-    hashed1 = tollBoothOperatorInstance.hashSecret(secret1)
+    await tollBoothOperatorInstance.addTollBooth(booth, {
+      from: owner1
+    })
+    await tollBoothOperatorInstance.addTollBooth(booth1, {
+      from: owner1
+    })
+    await tollBoothOperatorInstance.setMultiplier(vehicleType, multiplier, {
+      from: owner1
+    })
+    await tollBoothOperatorInstance.setMultiplier(vehicleType1, multiplier1, {
+      from: owner1
+    })
+    await tollBoothOperatorInstance.setRoutePrice(booth, booth1, price, {
+      from: owner1
+    })
+    await tollBoothOperatorInstance.setPaused(false, {
+      from: owner1
+    })
+    hashed = await tollBoothOperatorInstance.hashSecret(secret)
+    hashed1 = await tollBoothOperatorInstance.hashSecret(secret1)
   })
 
-  it('Given routeprice when is equal to deposit then no refund', () => {
+  it('1 - Given routeprice when is equal to deposit then no refund', async () => {
     //given
+    await tollBoothOperatorInstance.enterRoad(booth, hashed, {
+      from: vehicle,
+      value: (deposit * multiplier)
+    })
+    await tollBoothOperatorInstance.setRoutePrice(booth, booth1, deposit, {
+      from: owner1
+    })
 
     // when
-    const transaction = tollBoothOperatorInstance
-    .enterRoad
-    .call(booth, hashed, { from: vehicle, value: (deposit * multiplier) })
-    const events = transaction.logs
-    console.log(transaction)
-
+    const exitRoadTransaction = await tollBoothOperatorInstance.reportExitRoad(secret, {
+      from: booth1
+    })
+    const transactionLogs = exitRoadTransaction.logs
     // then
-    assert.equal(events.length, 1, 'Wrong number of events')
-    
+    assert.equal(transactionLogs.length, 1, 'Wrong number of events')
+    assert.equal(transactionLogs[0].event, 'LogRoadExited', 'Event name mismatched')
+    assert.equal(transactionLogs[0].args.refundWeis.toNumber(), 0, 'Refund weis not correct')
   })
 
-  it('Given routeprice when is less than deposit then no refund', async () => {
+  it('2 - Given routeprice when is less than deposit then no refund', async () => {
     //given
-
-
+    await tollBoothOperatorInstance.enterRoad(booth, hashed, {
+      from: vehicle,
+      value: (deposit * multiplier)
+    })
+    await tollBoothOperatorInstance.setRoutePrice(booth, booth1, deposit * 2, {
+      from: owner1
+    })
 
     // when
-    const transaction = await tollBoothOperatorInstance
-    .enterRoad(booth, hashed, { from: vehicle, value: (deposit * multiplier) })
-    const events = transaction.logs
-
+    const exitRoadTransaction = await tollBoothOperatorInstance.reportExitRoad(secret, {
+      from: booth1
+    })
+    const transactionLogs = exitRoadTransaction.logs
     // then
-    assert.equal(events.length, 1, 'Wrong number of events')
-    
+    assert.equal(transactionLogs.length, 1, 'Wrong number of events')
+    assert.equal(transactionLogs[0].event, 'LogRoadExited', 'Event name mismatched')
+    assert.equal(transactionLogs[0].args.refundWeis.toNumber(), 0, 'Refund weis not correct')
   })
 
-  it('Given routeprice when is bigger than deposit then refund is issued', () => {
+  it('3 - Given routeprice when is bigger than deposit then refund is issued', async () => {
     //given
-
-
+    await tollBoothOperatorInstance.enterRoad(booth, hashed, {
+      from: vehicle,
+      value: (deposit * multiplier)
+    })
+    await tollBoothOperatorInstance.setRoutePrice(booth, booth1, deposit - 3, {
+      from: owner1
+    })
 
     // when
-    const transaction = tollBoothOperatorInstance
-    .enterRoad
-    .call(booth, hashed, { from: vehicle, value: (deposit * multiplier) })
-    const events = transaction.logs
-
+    const exitRoadTransaction = await tollBoothOperatorInstance.reportExitRoad(secret, {
+      from: booth1
+    })
+    const transactionLogs = exitRoadTransaction.logs
     // then
-    assert.equal(events.length, 1, 'Wrong number of events')
-    
+    assert.equal(transactionLogs.length, 1, 'Wrong number of events')
+    assert.equal(transactionLogs[0].event, 'LogRoadExited', 'Event name mismatched')
+    assert.equal(transactionLogs[0].args.refundWeis.toNumber(), 3 * multiplier, 'Refund weis not correct')
   })
 
-  it('Given amount deposited when is greater than deposit and routeprice is equal to deposit then refund is issued', () => {
+  it('4 - Given amount deposited when is greater than deposit and routeprice is equal to deposit then refund is issued', async () => {
     //given
+    await tollBoothOperatorInstance.enterRoad(booth, hashed, {
+      from: vehicle,
+      value: (deposit * multiplier + 5 * multiplier)
+    })
+    await tollBoothOperatorInstance.setRoutePrice(booth, booth1, deposit, {
+      from: owner1
+    })
 
     // when
-    const transaction = tollBoothOperatorInstance
-    .enterRoad
-    .call(booth, hashed, { from: vehicle, value: (deposit * multiplier) })
-    const events = transaction.logs
-
+    const exitRoadTransaction = await tollBoothOperatorInstance.reportExitRoad(secret, {
+      from: booth1
+    })
+    const transactionLogs = exitRoadTransaction.logs
     // then
-    assert.equal(events.length, 1, 'Wrong number of events')
-    
+    assert.equal(transactionLogs.length, 1, 'Wrong number of events')
+    assert.equal(transactionLogs[0].event, 'LogRoadExited', 'Event name mismatched')
+    assert.equal(transactionLogs[0].args.refundWeis.toNumber(), 5 * multiplier, 'Refund weis not correct')
   })
 
-  it('Given amount deposited when is greater than deposit and routeprice is greater than deposit then refund is issued', () => {
+  it('5 - Given amount deposited when is greater than deposit and routeprice is greater than deposit then refund is issued', async () => {
     //given
-
-
+    await tollBoothOperatorInstance.enterRoad(booth, hashed, {
+      from: vehicle,
+      value: (deposit * multiplier + 5 * multiplier)
+    })
+    await tollBoothOperatorInstance.setRoutePrice(booth, booth1, deposit + 1, {
+      from: owner1
+    })
 
     // when
-    const transaction = tollBoothOperatorInstance
-    .enterRoad
-    .call(booth, hashed, { from: vehicle, value: (deposit * multiplier) })
-    const events = transaction.logs
-
+    const exitRoadTransaction = await tollBoothOperatorInstance.reportExitRoad(secret, {
+      from: booth1
+    })
+    const transactionLogs = exitRoadTransaction.logs
     // then
-    assert.equal(events.length, 1, 'Wrong number of events')
-    
+    assert.equal(transactionLogs.length, 1, 'Wrong number of events')
+    assert.equal(transactionLogs[0].event, 'LogRoadExited', 'Event name mismatched')
+    assert.equal(transactionLogs[0].args.refundWeis.toNumber(), 4 * multiplier, 'Refund weis not correct')
   })
 
-  it('Given two vehicles when enter road then fifo queue case goes successfully', () => {
+  it('Given two vehicles when enter road then fifo queue case goes successfully', async () => {
     //given
-
-
-
+    await tollBoothOperatorInstance.enterRoad(booth1, hashed, {
+      from: vehicle,
+      value: (deposit * multiplier + 5 * multiplier)
+    })
+    await tollBoothOperatorInstance.reportExitRoad(secret, {
+      from: booth
+    })
+    await tollBoothOperatorInstance.enterRoad(booth1, hashed1, {
+      from: vehicle1,
+      value: (deposit * multiplier1)
+    })
+    await tollBoothOperatorInstance.reportExitRoad(secret1, {
+      from: booth
+    })
     // when
-    const transaction = tollBoothOperatorInstance
-    .enterRoad
-    .call(booth, hashed, { from: vehicle, value: (deposit * multiplier) })
-    const events = transaction.logs
-
+    const exitRoadTransactionTwo = await tollBoothOperatorInstance.setRoutePrice(booth1, booth, deposit + 1, {
+      from: owner1
+    })
+    const logs = exitRoadTransactionTwo.logs
+    await tollBoothOperatorInstance.clearSomePendingPayments(booth1, booth, 1)
     // then
-    assert.equal(events.length, 1, 'Wrong number of events')
-    
+    assert.equal(logs.length, 2, 'Event count mismatched')
   })
 })
