7023bab1ad23bd7f63541cead02507372a783130
Wojciech Kalinski <wojciech.kalinski@espeo.eu>
2019-01-29 20:43:25 +0100

    Final fixes

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,149 +1,92 @@
-  // Import the page's CSS. Webpack will know what to do with it.
-  import '../styles/app.css'
+import Web3 from 'web3'
+import {
+  default as contract
+} from 'truffle-contract'
+import multiplierArtifacts from '../../build/contracts/MultiplierHolder.json'
+import regulatorArtifacts from '../../build/contracts/Regulator.json'
+import routePriceHolderArtifacts from '../../build/contracts/RoutePriceHolder.json'
+import tollBoothOperatorArtifacts from '../../build/contracts/TollBoothOperator.json'
+import tollBoothHolderArtifacts from '../../build/contracts/TollBoothHolder.json'
 
-  // Import libraries we need
-  import {
-    default as Web3
-  } from 'web3'
-  import {
-    default as contract
-  } from 'truffle-contract'
-  // Import our contract artifacts and turn them into usable abstractions.
-  import multiplierArtifacts from '../../build/contracts/MultiplierHolder.json'
-  import regulatorArtifacts from '../../build/contracts/Regulator.json'
-  import routePriceHolderArtifacts from '../../build/contracts/RoutePriceHolder.json'
-  import tollBoothOperatorArtifacts from '../../build/contracts/TollBoothOperator.json'
-  import tollBoothHolderArtifacts from '../../build/contracts/TollBoothHolder.json'
 
-  // MetaCoin is our usable abstraction, which we'll use through the code below.
+let MultiplierHolder = contract(multiplierArtifacts)
+let Regulator = contract(regulatorArtifacts)
+let RoutePriceHolder = contract(routePriceHolderArtifacts)
+let TollBoothOperator = contract(tollBoothOperatorArtifacts)
+let TollBoothHolder = contract(tollBoothHolderArtifacts)
+const App = {
+  web3: null,
+  account: null,
+  meta: null,
 
-  var MultiplierHolder = contract(multiplierArtifacts)
-  var Regulator = contract(regulatorArtifacts)
-  var RoutePriceHolder = contract(routePriceHolderArtifacts)
-  var TollBoothOperator = contract(tollBoothOperatorArtifacts)
-  var TollBoothHolder = contract(tollBoothHolderArtifacts)
+  start: async function() {
+    const { web3 } = this
 
-  console.log(JSON.stringify(Web3))
+      // get contract instance
+      const networkId = await web3.eth.net.getId()
+      web3 = await new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
 
-  // The following code is simple to show off interacting with your contracts.
-  // As your needs grow you will likely need to change its form and structure.
-  // For application bootstrapping, check out window.addEventListener below.
-  var accounts
-  var account
+  },
 
-  window.App = {
+  setVehicleType: async () => {
+    let vehicleType = parseInt(document.getElementById('vehicleType').value)
+    let recipient = document.getElementById('address').value
+    let regulatorInstance
 
-      start: function () {
-        var self = this
-
-        setVehicleType: () => {
-            var self = this
-
-            let vehicleType = parseInt(document.getElementById('vehicleType').value)
-            let recipient = document.getElementById('address').value
-            let regulatorInstance
-
-            if (vehicleType > 3 || vehicleType < 0) {
-              console.log('Error')
-              self.setStatus('Vehicle type value must be between 0 and 3')
-              return 0
-            }
-            Regulator.deployed().then((instance) => {
-              regulatorInstance = instance
-              return regulatorInstance.setVehicleType(vehicleType, recipient, {
-                from: account
-              })
-            }).then(() => {
-              self.setStatus('Vehicle type set')
-              self.refreshBalance()
-            }).catch((e) => {
-              console.log(e)
-              self.setStatus('An error occured while setting vehicle type')
-            })
-          }
-
-          createNewOperator: () => {
-            var self = this
-
-            let deposit = parseInt(document.getElementById('deposit').value)
-            let newOperatorAddress = document.getElementById('newOperatorAddress').value
-
-            let regulatorInstance
-            Regulator.deployed().then((instance) => {
-              regulatorInstance = instance
-              return regulatorInstance.createNewOperator(newOperatorAddress, deposit, {
-                from: account
-              })
-            }).then(() => {
-              self.setStatus('Vehicle type set')
-              self.refreshBalance()
-            }).catch((e) => {
-              console.log(e)
-              self.setStatus('An error occured while setting vehicle type')
-            })
-          }
-
-          addTollBooth: () => {
-            var self = this
-
-            let createdTollBoothAddress = document.getElementById('createdTollBooth').value
-
-            let tollBoothHolderInstance
-            TollBoothHolder.deployed().then((instance) => {
-              tollBoothHolderInstance = instance
-              return tollBoothHolderInstance.addTollBooth(createdTollBoothAddress, {
-                from: account
-              })
-            }).then(() => {
-              self.setStatus('New toll booth added: ' + createdTollBoothAddress)
-              self.refreshBalance()
-            }).catch((e) => {
-              console.log(e)
-              self.setStatus('An error occured while setting vehicle type')
-            })
-          }
-
-          setRoutePrice: () => {
-            var self = this
-
-            let entryBooth = document.getElementById('entryBooth').value
-            let exitBooth = document.getElementById('exitBooth').value
-            let priceWeis = parseInt(document.getElementById('priceWeis').value)
-
-            let routePriceHolderInstance
-            RoutePriceHolder.deployed().then((instance) => {
-              routePriceHolderInstance = instance
-              return routePriceHolderInstance.setRoutePrice(entryBooth, exitBooth, priceWeis, {
-                from: account
-              })
-            }).then(() => {
-              self.setStatus('Vehicle type set')
-              self.refreshBalance()
-            }).catch((e) => {
-              console.log(e)
-              self.setStatus('An error occured while setting vehicle type')
-            })
-          }
-
-          setMultiplier: () => {
-            var self = this
-
-            let vehicleType = parseInt(document.getElementById('multiplierVehicleType').value)
-            let multiplier = parseInt(document.getElementById('multiplier').value)
-
-            let multiplierInstance
-            MultiplierHolder.deployed().then((instance) => {
-              multiplierInstance = instance
-              return multiplierInstance.setMultiplier(vehicleType, multiplier, {
-                from: account
-              })
-            }).then(() => {
-              self.setStatus('Multiplier set')
-              self.refreshBalance()
-            }).catch((e) => {
-              console.log(e)
-              self.setStatus('An error occured while setting multiplier type')
-            })
-          }
+    if (vehicleType > 3 || vehicleType < 0) {
+        console.log('Error')
+        self.setStatus('Vehicle type value must be between 0 and 3')
+        return 0
       }
-    }
+    Regulator.deployed().then((instance) => {
+        regulatorInstance = instance
+        return regulatorInstance.setVehicleType(vehicleType, recipient, {
+          from: account
+        })
+      }).then(() => {
+        self.setStatus('Vehicle type set')
+        self.refreshBalance()
+      }).catch((e) => {
+          console.log(e)
+          self.setStatus('An error occured while setting vehicle type')
+        })
+  },
+
+  sendCoin: async function() {
+    const amount = parseInt(document.getElementById("amount").value)
+    const receiver = document.getElementById("receiver").value
+
+    this.setStatus("Initiating transaction... (please wait)")
+
+    const { sendCoin } = this.meta.methods
+    await sendCoin(receiver, amount).send({ from: this.account })
+
+    this.setStatus("Transaction complete!")
+    this.refreshBalance()
+  },
+
+  setStatus: function(message) {
+    const status = document.getElementById("status")
+    status.innerHTML = message
+  },
+}
+
+window.App = App
+
+window.addEventListener("load", function() {
+  if (window.ethereum) {
+    // use MetaMask's provider
+    App.web3 = new Web3(window.ethereum)
+    window.ethereum.enable() // get permission to access accounts
+  } else {
+    console.warn(
+      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
+    )
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    App.web3 = new Web3(
+      new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
+    )
+  }
+
+  App.start()
+})
--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,5 @@ module.exports = {
     filename: 'app.js',
     path: path.resolve(__dirname, 'script'),
     publicPath: '/script/'
-  },
-  module: {
-    rules: [{
-      test: /\.css$/, // include .js files
-      use: [{
-        loader: 'css-loader'
-      }]
-    }]
   }
 }
