3cebdc2eb413b5ecc3a66f1268ba6d8326c208c5
Wojciech Kalinski <wojciech.kalinski@espeo.eu>
2019-02-04 00:33:18 +0100

    Added interfaces of further smart contracts functions, added Bluebird to promisify and fix issues with web3

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -8,7 +8,6 @@ import routePriceHolderArtifacts from '../../build/contracts/RoutePriceHolder.js
 import tollBoothOperatorArtifacts from '../../build/contracts/TollBoothOperator.json'
 import tollBoothHolderArtifacts from '../../build/contracts/TollBoothHolder.json'
 
-
 let MultiplierHolder = contract(multiplierArtifacts)
 let Regulator = contract(regulatorArtifacts)
 let RoutePriceHolder = contract(routePriceHolderArtifacts)
@@ -17,6 +16,13 @@ let TollBoothHolder = contract(tollBoothHolderArtifacts)
 
 const regulatorAddress = '0xc9ae4a38cec6999610951f4c7d7765aaa8ac4e29'
 
+let addressBalance = 0
+
+const Promise = require('bluebird')
+
+let regulatorContract
+let toolBoothOperatorContract
+
 const App = {
   web3: null,
   account: null,
@@ -24,7 +30,23 @@ const App = {
 
   start: async () => {
     const web3 = await getWeb3
+    
+    console.log('START: web3')
     console.log(web3)
+    Promise.promisifyAll(web3.web3.eth, {
+      suffix: 'Promise'
+    })
+    console.log('Regulator artifacts: ')
+    console.log(regulatorArtifacts.abi)
+    console.log('Gas estimate: ')
+    console.log('Web3 eth')
+    console.log(web3.web3.eth)
+    console.log('Regulator new')
+    console.log(regulatorContract)
+    console.log('Coinbase: ')
+    console.log(web3.web3.eth.accounts.length)
+    console.log('Regulator address')
+    console.log(web3.web3.eth.getCoinbase)
   },
 
   checkBalance: async () => {
@@ -32,24 +54,9 @@ const App = {
   },
 
   setVehicleType: async () => {
+    console.log(await web3.eth.getCoinbase)
     let vehicleType = parseInt(document.getElementById('vehicleType').value)
     let recipient = document.getElementById('address').value
-    console.log('Regulator artifacts: ')
-    console.log(regulatorArtifacts.abi)
-    let regulator = web3.eth.contract(JSON.stringify(regulatorArtifacts))
-    const regulatorContract = await contract({
-      abi: regulatorArtifacts.abi
-    })
-    regulatorContract.setProvider(web3.currentProvider)
-    console.log(regulatorContract)
-    console.log('Regulator abi: ')
-    console.log(regulator.abi)
-    console.log('Gas estimate: ')
-    console.log(web3.eth.gasEstimate)
-    console.log('Web3')
-    console.log(web3)
-    console.log('Regulator new')
-    console.log(regulatorContract)
     await regulatorContract.at(regulatorAddress)
       .then(instance => {
         instance.setVehicleType(vehicleType, recipient, {
@@ -86,13 +93,17 @@ const App = {
   },
 
   setRoutePrice: async () => {
-    const entryBooth = document.getElementById("entryBooth").value
-    const exitBooth = document.getElementById("exitBooth").value
-    const amount = parseInt(document.getElementById("routePriceAmount").value)
+    const entryBooth = document.getElementById('entryBooth').value
+    const exitBooth = document.getElementById('exitBooth').value
+    const amount = parseInt(document.getElementById('routePriceAmount').value)
+  },
+
+  reportExitRoad: async() => {
+    const individualVehicleAddress = document.getElementById('individualVehicleAddress').value
   },
 
   setStatus: (message) => {
-    const status = document.getElementById("status")
+    const status = document.getElementById('status')
     status.innerHTML = message
   },
 
@@ -104,20 +115,6 @@ const App = {
 
 window.App = App
 
-window.addEventListener("load", function() {
-  if (window.ethereum) {
-    // use MetaMask's provider
-    App.web3 = new Web3(window.ethereum)
-    window.ethereum.enable() // get permission to access accounts
-  } else {
-    console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
-    )
-    // fallbaxck - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
-    )
-  }
-
+window.addEventListener('load', () => {
   App.start()
 })
